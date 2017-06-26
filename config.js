7831c9ed3e2270e666daa2c58833c2ba5552b8b6
Gary Woodfine <gary.woodfine@threenine.co.uk>
2017-06-26 22:10:22 +0100

    tidy up for article

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // Random comment to ensure omitted
 //check to see it works now
 module.exports = {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key:'',
-    access_token_secret:''
+    consumer_key: 'HTcRjVKI0SI7ISbUXa0V8kk5y',
+    consumer_secret: '4MpOpTHSMahinJLKKzDz6aHhMP8Ehr8pq2mmNW7qFChbn9mtFL',
+    access_token_key:'2707455955-QOWbXGKZGAbW7dVN4COJ1dlyrIMXXoZBwUcMKdV',
+    access_token_secret:'mH16JLeSJWh2i4SXD8lrnzkAoaZYTZuiiL3CfXXicNYKH'
 }
